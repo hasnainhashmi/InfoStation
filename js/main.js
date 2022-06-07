5cd0567379abde4fe458efe652251ca57d67a3df
hasnainhashmi <59309374+hasnainhashmi@users.noreply.github.com>
2022-06-07 14:44:46 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,35 +1,51 @@
-
-setTimeout(()=>{
+setTimeout(() => {
     const navigation = document.querySelector(".links");
     let menu = document.querySelector(".menu");
-    
+
     menu.addEventListener("click", () => {
-      navigation.style.setProperty("--childenNumber", navigation.children.length);
-    
-      navigation.classList.toggle("active");
-      menu.classList.toggle("active");
+        navigation.style.setProperty("--childenNumber", navigation.children.length);
+
+        navigation.classList.toggle("active");
+        menu.classList.toggle("active");
     });
 
 
-},1000)
+}, 1000)
 
-
-
-
-
-
-
-
-
-window.addEventListener('scroll',function() {      
+window.addEventListener('scroll', function () {
     let scroll = this.scrollY;
-    console.log(this.scrollY);
     if (scroll > 0) {
         document.querySelector("header").classList.add("shadow-active");
-    }
-    else {
+    } else {
         document.querySelector("header").classList.remove("shadow-active");
     }
 });
 
+
+function openOtpForm()
+{
+    let email = document.querySelector("#email").value;
+    fetch(window.location.origin+"/infostation.com/sendOtp.php?email="+email)
+    .then(response => response.json())
+    .then(data => console.log(data));
+    document.querySelector("form .form-1").style.display="none";
+    document.querySelector("form .form-2").style.display="block";
+
+}
+
+function checkOtp()
+{
+    let otp = document.querySelector("#otp").value;
+    fetch(window.location.origin+'/infostation.com/checkOtp.php?otp='+otp)
+    .then(response => response.json())
+    .then(data => {
+        if(data == true)
+        {
+            document.querySelector("form").submit();
+        }
+        else{
+            document.querySelector(".form-2 p").style.display="block";
+        }
+    });
+}
 
